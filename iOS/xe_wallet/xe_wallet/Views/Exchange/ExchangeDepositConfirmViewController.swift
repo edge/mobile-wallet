540423c8b0c65e7ecb07d88e38a88c6afa9801c5
Paul Davis <pjdavis1970@gmail.com>
2021-11-08 23:10:11 +0000

    feat: added exchange deposit confirm ui code to handle display

---
+++ iOS/xe_wallet/xe_wallet/Views/Exchange/ExchangeDepositConfirmViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeDepositConfirmViewController.swift
+//  xe_wallet
+//
+//  Created by Paul Davis on 08/11/2021.
+//
+
+import UIKit
+
+class ExchangeDepositConfirmViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
---
+++ iOS/xe_wallet/xe_wallet/Views/Exchange/ExchangeWithdrawConfirmViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeWithdrawConfirmViewController.swift
+//  xe_wallet
+//
+//  Created by Paul Davis on 08/11/2021.
+//
+
+import UIKit
+
+class ExchangeWithdrawConfirmViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
